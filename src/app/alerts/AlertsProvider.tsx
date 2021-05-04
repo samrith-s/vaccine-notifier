1d6ceb331585d251f93154d8ae546d18d5439831
Samrith Shankar <samrith.298@gmail.com>
2021-05-04 09:45:31 +0530

    chore: fix lint issues

--- src/app/alerts/AlertsProvider.tsx
+++ src/app/alerts/AlertsProvider.tsx
@@ -18,6 +18,7 @@ export function AlertsProvider({ children }: AlertsProviderProps) {
 
     useEffect(() => {
         init();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
